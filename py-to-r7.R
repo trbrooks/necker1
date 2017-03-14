25b32efd3096d55d2921c97d684a44b3cbe09418
T.R. Brooks <thomas.brooks@uconn.edu>
2017-03-14 09:23:34 -0400

    Update py-to-r7.R

--- py-to-r7.R
+++ py-to-r7.R
@@ -2,7 +2,7 @@
 ## Un-nests psychopy data so it can be analyzed in R
 
 getwd()
-setwd("/Users/trbrooks/documents/past semesters/spring 2016/necker/data directory/necker_round2/data")
+setwd("/Users/trbrooks/Documents/necker/data directory/data")
 dir()->file.names
 grep("csv", file.names)->temp
 file.names[temp]->pydata1
